6d7669fdd7df540cc5ae61efc25ac070741c7c3b
HarryRamirez <kennyharry47@gmail.com>
2025-05-09 14:40:02 -0500

    Solucionado el coflicto

--- src/main/java/com/kenny/app/consultas/consultas_medicas/services/DoctorService.java
+++ src/main/java/com/kenny/app/consultas/consultas_medicas/services/DoctorService.java
@@ -1,7 +1,6 @@
 package com.kenny.app.consultas.consultas_medicas.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.kenny.app.consultas.consultas_medicas.dtos.DoctorRequestDTO;
 import com.kenny.app.consultas.consultas_medicas.dtos.DoctorResponseDTO;
@@ -9,8 +8,8 @@ import com.kenny.app.consultas.consultas_medicas.dtos.DoctorResponseDTO;
 public interface DoctorService {
 
     List<DoctorResponseDTO> findAll();
-    Optional<DoctorResponseDTO> findById(Long id);
+    DoctorResponseDTO findById(Long id);
     DoctorResponseDTO create(DoctorRequestDTO dto);
-    Optional<DoctorResponseDTO> update(Long id, DoctorRequestDTO dto);
+    DoctorResponseDTO update(Long id, DoctorRequestDTO dto);
     void delete(Long id);
 }
