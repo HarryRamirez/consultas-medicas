9f8cb1f5b2caa7c8dff26dab098290b21ace30b5
HarryRamirez <kennyharry47@gmail.com>
2025-05-13 20:22:25 -0500

    nada

--- src/main/java/com/kenny/app/consultas/consultas_medicas/repositories/ScheduleRepository.java
+++ src/main/java/com/kenny/app/consultas/consultas_medicas/repositories/ScheduleRepository.java
@@ -1,7 +1,6 @@
 package com.kenny.app.consultas.consultas_medicas.repositories;
 
 import java.time.LocalTime;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
